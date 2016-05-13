eb46b4beb11acbdfc5cc35a16b46e012dbbf0045
Travis Vignon <travis@lookitsatravis.com>
2016-05-13 13:12:16 -0500

    Added basic http spec file

---
+++ test/http.spec.js
@@ -0,0 +1,27 @@
+import chai from 'chai';
+import * as support from './support';
+import http from '../lib/api_guardian/io/http';
+
+let should = chai.should();
+
+describe('Io/Http', () => {
+  describe('.setAccessToken/.getAccessToken', () => {
+    it('should set/get the internal access token', () => {
+      let result = http.getAccessToken();
+      should.equal(result, null);
+
+      result = http.setAccessToken('test');
+
+      http.getAccessToken().should.equal('test');
+    });
+  });
+
+  describe('.postJson', () => {
+  });
+
+  describe('.fetchJson', () => {
+  });
+
+  describe('.$fetch', () => {
+  });
+});
