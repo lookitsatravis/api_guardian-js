6304b7fa0e96470b76d586db1266e73c937e0935
Travis Vignon <travis@lookitsatravis.com>
2016-05-25 17:36:19 -0500

    Added storage wrapper
    To prevent issues where localStorage doesn't work (specifically private Safari browsing). I expect that eventually it could fall back to a cookie instead of in-memory.

---
+++ test/storage.spec.js
@@ -0,0 +1,93 @@
+import chai from 'chai';
+import * as support from './support';
+import storage from '../lib/api_guardian/storage';
+
+let expect = chai.expect;
+let should = chai.should();
+
+describe('Storage', () => {
+  beforeEach(() => {
+    storage.clear();
+  });
+
+  describe('.getItem/.setItem', () => {
+    it('sets and returns value by key', () => {
+      should.equal(storage.getItem('foo'), null);
+
+      storage.setItem('foo', 'bar');
+
+      storage.getItem('foo').should.equal('bar');
+    });
+
+    it('should work when local storage is not available', () => {
+      disableLocalStorage();
+
+      (() => storage.setItem('foo', 'bar')).should.not.throw(Error);
+      (() => storage.getItem('foo')).should.not.throw(Error);
+      storage.getItem('foo').should.equal('bar');
+
+      enableLocalStorage();
+    });
+  });
+
+  describe('.clear', () => {
+    it('should clear stored data', () => {
+      testStorageClear();
+    });
+
+    it('should work when local storage is not available', () => {
+      disableLocalStorage();
+
+      testStorageClear();
+
+      enableLocalStorage();
+    });
+  });
+});
+
+function testStorageClear() {
+  storage.setItem('foo', 'bar');
+  storage.setItem('baz', 'qux');
+  storage.getItem('foo').should.equal('bar');
+  storage.getItem('baz').should.equal('qux');
+
+  storage.clear();
+
+  // Could be either value for some reason. Weird.
+  expect(storage.getItem('foo')).to.be.oneOf([null, undefined]);
+  expect(storage.getItem('baz')).to.be.oneOf([null, undefined]);
+}
+
+let origLocalStorage = localStorage;
+
+function disableLocalStorage() {
+  let newLocalStorage = Object.assign({}, localStorage);
+
+  Object.defineProperty(newLocalStorage, 'getItem', {
+    get: () => {
+      throw new Error('Ah, ah, ah. You didn\'t say the magic word!');
+    }
+  });
+
+  Object.defineProperty(newLocalStorage, 'setItem', {
+    get: () => {
+      throw new Error('Houston, we have a problem.');
+    }
+  });
+
+  Object.defineProperty(newLocalStorage, 'clear', {
+    get: () => {
+      throw new Error('Some great movie quote for problems.');
+    }
+  });
+
+  localStorage = newLocalStorage;
+
+  (() => localStorage.getItem('foo')).should.throw(Error);
+  (() => localStorage.setItem('foo', 'bar')).should.throw(Error);
+  (() => localStorage.clear()).should.throw(Error);
+}
+
+function enableLocalStorage() {
+  localStorage = origLocalStorage;
+}
